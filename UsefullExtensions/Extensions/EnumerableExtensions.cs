e6d377206e9df6a2fe148d83903009a093752c3e
Konstantin <kes8594@yandex.ru>
2020-06-04 23:49:08 +0300

    Add enumerable extensions

---
+++ UsefullExtensions/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace UsefulExtensions.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static string Join(this IEnumerable<string> enumerable, string separator) => string.Join(separator, enumerable);
+    }
+}
