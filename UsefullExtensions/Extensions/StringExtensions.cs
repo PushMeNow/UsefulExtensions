fef994a9861d1ee48cde93d2f1b48452eac26650
Konstantin <kes8594@yandex.ru>
2020-06-04 13:14:00 +0300

    Add project files.

---
+++ UsefullExtensions/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace UsefulExtensions.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNull(this string value) => value is null;
+
+        public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+
+        public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
+    }
+}
