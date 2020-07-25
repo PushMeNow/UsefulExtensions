8f178c2b2477453a990b1a9e0bedbbf6e62d56d0
Konstantin <33716293+PushMeNow@users.noreply.github.com>
2020-07-25 18:34:24 +0300

    Add foreach extention

--- UsefullExtensions/Extensions/EnumerableExtensions.cs
+++ UsefullExtensions/Extensions/EnumerableExtensions.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace UsefulExtensions.Extensions
 {
     public static class EnumerableExtensions
     {
         public static string ToSeparatedString(this IEnumerable<string> enumerable, string separator) => string.Join(separator, enumerable);
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T item in source)
+            {
+                action(item);
+            }
+        }
     }
 }
