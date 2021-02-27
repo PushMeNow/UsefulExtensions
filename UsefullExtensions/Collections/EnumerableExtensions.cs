71a9b84040d8eccb41661130479ab5c6e2a7f69b
Konstantin Borovik <konstantin.borovik@fortegrp.com>
2021-02-27 12:21:09 +0300

    - set lang version - change framework version - add IsEmpty and ToSeparatedString(for char type) extensions

--- UsefullExtensions/Collections/EnumerableExtensions.cs
+++ UsefullExtensions/Collections/EnumerableExtensions.cs
@@ -5,11 +5,19 @@ namespace UsefulExtensions.Collections
 {
     public static class EnumerableExtensions
     {
-        public static string ToSeparatedString(this IEnumerable<string> enumerable, string separator) => string.Join(separator, enumerable);
+        public static string ToSeparatedString(this IEnumerable<string> enumerable, string separator)
+        {
+            return string.Join(separator, enumerable);
+        }
+
+        public static string ToSeparatedString(this IEnumerable<string> stringEnum, char separator)
+        {
+            return string.Join(separator, stringEnum);
+        }
 
         public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
-            foreach (T item in source)
+            foreach (var item in source)
             {
                 action(item);
             }
---
+++ UsefullExtensions/Crossing/ObjectExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UsefulExtensions.Strings;
+
+namespace UsefulExtensions.Crossing
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsEmpty<T>(this T value)
+        {
+            if (EqualityComparer<T>.Default.Equals(value, default))
+            {
+                return true;
+            }
+
+            switch (value)
+            {
+                case string str when str.IsNullOrWhiteSpace():
+                    return true;
+                case ICollection { Count: 0 }:
+                case Array { Length: 0 }:
+                case IEnumerable e when !e.Cast<object>()
+                                          .Any():
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
